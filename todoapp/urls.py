c0e3c2e51b564138261b63eb9a75ae6a42355b2f
Pablo García <me.pablouser@gmail.com>
2023-07-25 20:28:30 +0200

    added login form

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('projects/', views.projects, name='projects'),
-    path('tasks/<int:project_id>', views.tasks, name='tasks'),
-    path('task/<int:id>', views.task, name='task'),
-    path('create_task/<int:project_id>', views.create_task, name='create_task'),
-    path('create_project/', views.create_project, name='create_task'),
-    path('delete_project/<int:id>', views.delete_project, name='delete_project'),
-    path('update_project/<int:id>', views.update_project, name='update_project'),
-    path('completed/<int:id>', views.completed, name='completed'),
-
+    path("", views.home, name="home"),
+    path("projects/", views.projects, name="projects"),
+    path("tasks/<int:project_id>", views.tasks, name="tasks"),
+    path("task/<int:id>", views.task, name="task"),
+    path("create_task/<int:project_id>", views.create_task, name="create_task"),
+    path("create_project/", views.create_project, name="create_task"),
+    path("delete_project/<int:id>", views.delete_project, name="delete_project"),
+    path("update_project/<int:id>", views.update_project, name="update_project"),
+    path("completed/<int:id>", views.completed, name="completed"),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,72 +3,99 @@ from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
 from .forms import CreateTask, CreateProject, UpdateProject
 
+
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
 
 def projects(request):
     # projects = list(Project.objects.values())
     projects = Project.objects.all()
-    return render(request, 'projects.html', {
-        'projects': projects
-    })
+    return render(request, "projects.html", {"projects": projects})
+
 
 def tasks(request, project_id):
     tasks = Task.objects.all()
     project_tasks = tasks.filter(project_id=project_id)
     project = Project.objects.get(id=project_id)
-    return render(request, 'tasks.html', {
-        'project_tasks': project_tasks,
-        'project': project
-    })
+    return render(
+        request, "tasks.html", {"project_tasks": project_tasks, "project": project}
+    )
+
 
 def task(request, id):
     task = Task.objects.get(id=id)
-    return render(request, 'task.html', {
-        'task': task,
-    })
+    return render(request, "task.html", {"task": task})
+
 
 def create_task(request, project_id):
     project = Project.objects.get(id=project_id)
-    if request.method == 'GET':
-        return render(request, 'create_task.html', {
-            'form': CreateTask(),
-            'project': project,
-    })
+    if request.method == "GET":
+        return render(
+            request,
+            "create_task.html",
+            {
+                "form": CreateTask(),
+                "project": project,
+            },
+        )
     else:
-        Task.objects.create(title=request.POST['title'], description=request.POST['description'], project_id=project_id)
-        return redirect('/tasks/' + str(project_id))
-    
+        Task.objects.create(
+            title=request.POST["title"],
+            description=request.POST["description"],
+            project_id=project_id,
+        )
+        return redirect("/tasks/" + str(project_id))
+
+
 def create_project(request):
-    if request.method == 'GET':
-        return render(request, 'create_project.html', {
-            'form': CreateProject(),
-        })
+    if request.method == "GET":
+        return render(
+            request,
+            "create_project.html",
+            {
+                "form": CreateProject(),
+            },
+        )
     else:
-        Project.objects.create(name=request.POST['name'])
-        return redirect('projects')
-    
+        Project.objects.create(name=request.POST["name"])
+        return redirect("projects")
+
+
 def delete_project(request, id):
     project = Project.objects.get(id=id)
     project.delete()
-    return redirect('projects')
+    return redirect("projects")
+
 
 def update_project(request, id):
-    if request.method == 'GET':
+    if request.method == "GET":
         project = Project.objects.get(id=id)
-        return render(request, 'update_project.html', {
-            'form': UpdateProject(),
-            'project': project,
-        })
+        return render(
+            request,
+            "update_project.html",
+            {
+                "form": UpdateProject(),
+                "project": project,
+            },
+        )
     else:
         project = Project.objects.filter(id=id)
-        project.update(name=request.POST['name'])
-        return redirect('projects')
+        project.update(name=request.POST["name"])
+        return redirect("projects")
+
 
 def completed(request, id):
     task = Task.objects.filter(id=id)
     task.update(completed=True)
     task_object = Task.objects.get(id=id)
     fk = task_object.project
-    return redirect('/tasks/' + str(fk.id))
+    return redirect("/tasks/" + str(fk.id))
+
+
+def sign_up(request):
+    if request.method == "GET":
+        return render(request, "sign_up.html")
+    else:
+        form = RegisterForm(request.POST)
